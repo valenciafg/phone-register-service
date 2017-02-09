e95ffc207539a66a3e416e7c2b4162c54d1d49f6
Webmaster <webmaster@hotelplazameru.com>
2017-02-09 16:10:36 -0430

    new routes and controllers

--- database.example.js
+++ database.example.js
@@ -14,14 +14,26 @@ var sqlConnection = new sql.Connection(config);
 *   Handlers
 **/
 sqlConnection.on('connect', function(err) {
-  console.log('Database connection established');
+  console.log('Database connection established.',err);
 });
 sqlConnection.on('close', function(err) {
-  console.log('Database closed');
+  console.log('Database closed. ',err);
 });
 sqlConnection.on('error', function(err) {
   console.log('Database Error: ', err);
 });
+/**
+*   Promises Functions
+**/
+function responseRecordset(recordset,res){
+    res.send(recordset);
+}
+function logRecordset(recordset){
+    console.log(recordset);
+}
+function logError(err){
+    console.log('Error:',err);
+}
 /**
 *   Functions
 **/
@@ -33,15 +45,75 @@ var PhoneTypeList = function(){
             return recordset;
         });
     });
-}/*
-var LastPhoneCalls = ()=>{
-    sqlConnection.connect(function(err) {
-        var request = new sql.Request(sqlConnection);
-        request.query('SELECT TOP(100) * FROM PhoneCalls ORDER BY CreationDate DESC', function(err, recordset) {
-            console.log(recordset);
-        });
-    });
-}*/
+}
+
+var ExtensionList = function(response){
+    sql.connect(config)
+        .then(function(){
+            var request = new sql.Request();
+            request.query('SELECT * FROM Extensions')
+            .then(function(recordset){
+                console.log('Extension List ',recordset);
+                response.send(recordset);
+            })
+            .catch(logError);
+        }).catch(logError)
+}
+
+var SearchExtension = function(response,ext_number){
+    sql.connect(config)
+        .then(function(){
+            var request = new sql.Request();
+            request.input('ext_number', sql.NVarChar(50), ext_number)
+                .query('SELECT * FROM Extensions WHERE PhoneNumber = @ext_number')
+                .then(function(recordset){
+                    console.log('Extension List ',recordset);
+                    response.send(recordset);
+                })
+                .catch(logError);
+        })
+        .catch(logError)
+}
+
+var CallsList = function(response){
+    sql.connect(config)
+        .then(function(){
+            var request = new sql.Request();
+            request.query('SELECT * FROM PhoneCalls ORDER BY CallDate DESC')
+            .then(function(recordset){
+                console.log('Calls List ',recordset);
+                response.send(recordset);
+            })
+            .catch(logError);
+        }).catch(logError)
+}
+
+var LastPhoneCalls = function(response){
+    sql.connect(config)
+        .then(function(){
+            var request = new sql.Request();
+            request.query('SELECT TOP(100) * FROM PhoneCalls ORDER BY CallDate DESC')
+            .then(function(recordset){
+                console.log('Last Calls ',recordset);
+                response.send(recordset);
+            })
+            .catch(logError);
+        }).catch(logError)
+}
+var searchCallsByExtension = (response, ext_number) => {
+    sql.connect(config)
+        .then(function(){
+            var request = new sql.Request();
+            request.input('ext_number', sql.NVarChar(50), ext_number)
+            .query('SELECT * FROM PhoneCalls WHERE PhoneExtension = @ext_number ORDER BY CallDate DESC')
+            .then(function(recordset){
+                console.log('Calls by ext '+ext_number,recordset);
+                response.send(recordset);
+            })
+            .catch(logError);
+        })
+        .catch(logError)
+}
 var RegisterCall = function(data){
     var sql = 'INSERT INTO PhoneCalls '+
     '(PhoneExtension,PhoneDestination,PhoneCallStartTime,PhoneCallDuration,RegisteredCost,CostCenterName,BusinessCodeNumber,TrfSub,CommunicationType,CommunicationTypeTwo,CallType)'+
@@ -58,7 +130,6 @@ var RegisterCall = function(data){
     '\',\''+data.commType2+
     '\',\''+data.callType+
     '\')';
-    //console.log(sql);
     sqlConnection.connect(function(err) {
         var transaction = sqlConnection.transaction();
         transaction.begin(function(err) {
@@ -79,5 +150,11 @@ var RegisterCall = function(data){
 **/
 module.exports = {
     PhoneTypeList: PhoneTypeList,
-    RegisterCall: RegisterCall
+    RegisterCall: RegisterCall,
+    ExtensionList: ExtensionList,
+    LastPhoneCalls: LastPhoneCalls,
+    searchCallsByExtension: searchCallsByExtension,
+    CallsList: CallsList,
+    SrvConfig: config,
+    sqlConnection: sqlConnection
 }
--- server.js
+++ server.js
@@ -2,14 +2,34 @@ var express = require('express');
 var app = express();
 var server = require('http').Server(app);
 var io = require('socket.io')(server);
+var database = require('./database');
+var sql = require('mssql');
+var bodyParser = require('body-parser');
 
 const SALUDO = "Bienvenido al servicio de captura de llamada, si usted puede ver este mensaje es porque el servicio está activo";
-
+//  Express server uses
 app.use(express.static('public'));
+app.use(bodyParser.json()); // support json encoded bodies
+app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
+/**
+*   Routes
+**/
 app.get('/', function(req, res){
-  res.status(200).send(SALUDO);
+    res.status(200).send(SALUDO);
 });
-
+app.get('/extension', function(req, res) {
+    database.ExtensionList(res);
+});
+app.get('/lastcalls', function(req, res) {
+    database.LastPhoneCalls(res);
+});
+app.get('/call', function(req, res) {
+    database.CallsList(res);
+});
+app.get('/call/:ext', function(req, res) {
+    database.searchCallsByExtension(res,req.params.ext);
+});
+//  Handle new socket.io client connected
 io.on('connection', function(socket){
     var address = socket.handshake.address;
     console.log('Se ha conectado un cliente ',address);
@@ -18,6 +38,7 @@ io.on('connection', function(socket){
 io.on('disconnect', function () {
     console.log('Cliente desconectado');
 });
+//Server Listen port
 server.listen(8080, function(){
   console.log("Servidor corriendo en http://localhost:8080");
 });
